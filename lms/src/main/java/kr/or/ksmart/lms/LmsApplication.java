491b7337fd5a5a38ebb58c314beca491d7445ba0
Han <Han@118.43.36.200>
2019-06-14 20:09:42 +0900

    [한병권] war배포 설정 19-06-14

--- lms/src/main/java/kr/or/ksmart/lms/LmsApplication.java
+++ lms/src/main/java/kr/or/ksmart/lms/LmsApplication.java
@@ -2,10 +2,17 @@ package kr.or.ksmart.lms;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class LmsApplication {
+public class LmsApplication extends SpringBootServletInitializer {
 
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(LmsApplication.class);
+	}
+	
 	public static void main(String[] args) {
 		System.out.println("=======================tomcat 실행 전==========================");
 		SpringApplication.run(LmsApplication.class, args);
