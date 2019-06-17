3417a0a8412650fdab4bb3a0ddb60cd2058b1288
smart-202-15 <smart-202-15@smart-202-15-PC>
2019-06-17 17:02:04 +0900

    [나현주]파일경로 수정 후 merge

--- lms/src/main/java/kr/or/ksmart/lms/subject/mapper/SubjectMapper.java
+++ lms/src/main/java/kr/or/ksmart/lms/subject/mapper/SubjectMapper.java
@@ -2,8 +2,10 @@ package kr.or.ksmart.lms.subject.mapper;
 
 import java.util.List;
 
-import kr.or.ksmart.lms.subject.vo.InfoSubject;
+import org.apache.ibatis.annotations.Mapper;
 
+import kr.or.ksmart.lms.subject.vo.InfoSubject;
+@Mapper
 public interface SubjectMapper {
 	public List<InfoSubject> subjectList ();
 }
