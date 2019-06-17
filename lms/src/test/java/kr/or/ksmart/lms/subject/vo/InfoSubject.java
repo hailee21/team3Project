1c3a4d99741e3853f30ae2b4a4e34725d738e4ab
smart-202-15 <smart-202-15@smart-202-15-PC>
2019-06-17 16:04:37 +0900

    [나현주] merge test

---
+++ lms/src/test/java/kr/or/ksmart/lms/subject/vo/InfoSubject.java
@@ -0,0 +1,48 @@
+package kr.or.ksmart.lms.subject.vo;
+
+public class InfoSubject {
+	String infoSubjectCode;
+	String infoLectureCode;
+	String infoSubjectName;
+	int infoSubjectDifficulty;
+	String infoSubjectModificationHistory;
+	
+	public String getInfoSubjectCode() {
+		return infoSubjectCode;
+	}
+	public void setInfoSubjectCode(String infoSubjectCode) {
+		this.infoSubjectCode = infoSubjectCode;
+	}
+	public String getInfoLectureCode() {
+		return infoLectureCode;
+	}
+	public void setInfoLectureCode(String infoLectureCode) {
+		this.infoLectureCode = infoLectureCode;
+	}
+	public String getInfoSubjectName() {
+		return infoSubjectName;
+	}
+	public void setInfoSubjectName(String infoSubjectName) {
+		this.infoSubjectName = infoSubjectName;
+	}
+	public int getInfoSubjectDifficulty() {
+		return infoSubjectDifficulty;
+	}
+	public void setInfoSubjectDifficulty(int infoSubjectDifficulty) {
+		this.infoSubjectDifficulty = infoSubjectDifficulty;
+	}
+	public String getInfoSubjectModificationHistory() {
+		return infoSubjectModificationHistory;
+	}
+	public void setInfoSubjectModificationHistory(String infoSubjectModificationHistory) {
+		this.infoSubjectModificationHistory = infoSubjectModificationHistory;
+	}
+	
+	@Override
+	public String toString() {
+		return "InfoSubject [infoSubjectCode=" + infoSubjectCode + ", infoLectureCode=" + infoLectureCode
+				+ ", infoSubjectName=" + infoSubjectName + ", infoSubjectDifficulty=" + infoSubjectDifficulty
+				+ ", infoSubjectModificationHistory=" + infoSubjectModificationHistory + "]";
+	}
+	
+}
