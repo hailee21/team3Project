464dc815abb92bc641224536fed0204120ce1ea3
smart-202-15 <smart-202-15@smart-202-15-PC>
2019-06-19 17:29:43 +0900

    [나현주] 회원등록 MemberInsertForm.html작성 LEJoin.html 작성중

--- lms/src/main/java/kr/or/ksmart/lms/member/mapper/MemberMapper.java
+++ lms/src/main/java/kr/or/ksmart/lms/member/mapper/MemberMapper.java
@@ -6,7 +6,10 @@ import kr.or.ksmart.lms.index.vo.IndexInstitution;
 import kr.or.ksmart.lms.member.vo.Member;
 @Mapper
 public interface MemberMapper {
+	//	협회코드
 	public IndexInstitution selectInstitution(String institutionCode);
-
+	//	신규회원등록
 	public int insertMember(Member member);
+	//	회원코드 PK 출력mapper
+	public String selectMemberCodePk();
 }
