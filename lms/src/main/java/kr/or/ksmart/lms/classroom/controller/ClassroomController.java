d85ea9199a2e3a00dabd68a98b16f2ff4cef583c
Administrator <Administrator@MSDN-SPECIAL>
2019-06-21 17:55:14 +0900

    [유하람] 강의실등록 진행중
    - SearchInstitution.html 에서 받은 institutionCode로 addClassroom-post메서드 실행
    안됨

--- lms/src/main/java/kr/or/ksmart/lms/classroom/controller/ClassroomController.java
+++ lms/src/main/java/kr/or/ksmart/lms/classroom/controller/ClassroomController.java
@@ -1,11 +1,15 @@
 package kr.or.ksmart.lms.classroom.controller;
 
+import java.util.Map;
+
 import javax.servlet.http.HttpSession;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
 
 import kr.or.ksmart.lms.classroom.service.ClassroomService;
@@ -15,40 +19,7 @@ import kr.or.ksmart.lms.classroom.vo.Classroom;
 public class ClassroomController {
 	@Autowired ClassroomService classroomService;
 	// 협회
-	// association layout 강의실 추가 controller
-	@GetMapping("/association/classroom/addClassroom")
-	public ModelAndView addClassroom(ModelAndView mav, HttpSession session) {
-		String memberRank = (String)session.getAttribute(("memberRank"));
-		if(memberRank.equals("협회직원")) {
-			System.out.println("협회직원");
-			
-			System.out.println("[ClassroomController addClassroom]");
-			mav.setViewName("/association/classroom/addClassroom");
-		}else {
-			System.out.println("협회직원아님");
-			
-			mav.setViewName("/association/associationLogin");
-		}
-		return mav;
-	}
-	// 강의실 추가 처리 
-	@PostMapping("/association/classroom/addClassroom")
-	public ModelAndView addClassroom(ModelAndView mav, HttpSession session, Classroom classroom) {
-		String memberRank = (String)session.getAttribute(("memberRank"));
-		if(memberRank.equals("협회직원")) {
-			System.out.println("협회직원");
-			
-			System.out.println("[ClassroomController addClassroom]");
-			classroomService.addClassroom(classroom);
-			mav.setViewName("/association/classroom/classroomList");
-		}else {
-			System.out.println("협회직원아님");
-			
-			mav.setViewName("/association/associationLogin");
-		}
-		return mav;
-	}
-	// 교육원 검색
+	// association layout 교육원 검색 controller 
 	@GetMapping("/association/classroom/searchInstitution")
 	public ModelAndView getInstitutionList(ModelAndView mav, HttpSession session) {
 		String memberRank = (String)session.getAttribute(("memberRank"));
@@ -57,6 +28,50 @@ public class ClassroomController {
 			
 			System.out.println("[ClassroomController getInstitutionList 교육원 검색]");
 			mav.setViewName("/association/classroom/searchInstitution");
+		}else {
+			System.out.println("협회직원아님");
+			
+			mav.setViewName("/association/associationLogin");
+		}
+		return mav;
+	}
+	
+	// association layout 강의실 추가 controller
+	@GetMapping("/association/classroom/addClassroom")
+	public ModelAndView addClassroom(ModelAndView mav, HttpSession session, Model model
+									,@RequestParam String instCode) {
+		String memberRank = (String)session.getAttribute(("memberRank"));
+		if(memberRank.equals("협회직원")) {
+			System.out.println("협회직원");
+			
+			System.out.println("[ClassroomController addClassroom]");
+			mav.setViewName("/association/classroom/addClassroom");
+			
+			// map에 담아서 가져온 값들을 model에 넣어 view에서 활용
+			Map<String, Object> map = classroomService.getInstitutionByInstCode(instCode);	
+			model.addAttribute("instCode", map.get("instCode"));
+			model.addAttribute("instName", map.get("instName"));
+			model.addAttribute("instLocation", map.get("instLocation"));
+			model.addAttribute("useList", map.get("useList"));
+		}else {
+			System.out.println("협회직원아님");
+			
+			mav.setViewName("/association/associationLogin");
+		}
+		return mav;
+	}
+	// 강의실 추가  
+	@PostMapping("/association/classroom/addClassroom")
+	public ModelAndView addClassroom(ModelAndView mav, HttpSession session, Classroom classroom
+									,@RequestParam String institutionCode) {
+		String memberRank = (String)session.getAttribute(("memberRank"));
+		if(memberRank.equals("협회직원")) {
+			System.out.println("협회직원");
+			
+			System.out.println("[ClassroomController addClassroom]institutionCode: "+institutionCode);
+						
+			classroomService.addClassroom(classroom, institutionCode);
+			mav.setViewName("/association/classroom/classroomList");
 		}else {
 			System.out.println("협회직원아님");
 			
--- lms/src/main/java/kr/or/ksmart/lms/classroom/service/ClassroomService.java
+++ lms/src/main/java/kr/or/ksmart/lms/classroom/service/ClassroomService.java
@@ -1,6 +1,8 @@
 package kr.or.ksmart.lms.classroom.service;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -14,14 +16,8 @@ import kr.or.ksmart.lms.lecture.mapper.LectureMapper;
 public class ClassroomService {
 	@Autowired ClassroomMapper classroomMapper;
 	@Autowired LectureMapper lectureMapper;
-	
-	// 강의실 추가 메서드
-	public int addClassroom(Classroom classroom) {
-		System.out.println("[ClassroomService addClassroom]");
-		return classroomMapper.insertClassroom(classroom);
-	}
-	
-	// 교육원 리스트 검색, 출력 메서드
+		
+	// 교육원 리스트 검색, 출력 
 	public List<Institution> getInstitutionList(String instName){
 		System.out.println("[ClassroomService getInstitutionList]");
 		System.out.println("[ClassroomService getInstitutionList] instName :"+instName);
@@ -35,4 +31,58 @@ public class ClassroomService {
 		
 		return instListByName;
 	}
+	
+	// 교육원명, 위치 select -> addClassroom.html에 입력 
+	public Map<String, Object> getInstitutionByInstCode(String instCode){
+		System.out.println("[ClassroomService getInstitutionByInstCode]");
+		System.out.println("[ClassroomService getInstitutionByInstCode] instCode : "+instCode);
+		
+		// mapper에서 가져온 교육원명, 위치
+		Institution institution = new Institution();
+		institution = classroomMapper.selectInstitutionByInstitutionCode(instCode);
+		String instName = institution.getInstitutionName();
+		String instLocation = institution.getInstitutionLocation();
+		System.out.println("[ClassroomService getInstitutionByInstCode] instName : "+instName);
+		System.out.println("[ClassroomService getInstitutionByInstCode] instLocation : "+instLocation);
+		// mapper에서 가져온 강의실용도 리스트
+		List<Classroom> list = classroomMapper.selectClassroomUse();
+		System.out.println("[ClassroomService getInstitutionByInstCode] list : "+list);
+		
+		// mapper에서 가져온 값 map에 담아서 이동
+		Map<String, Object> map = new HashMap<String, Object>();
+		map.put("instName", instName);
+		map.put("instLocation", instLocation);
+		map.put("useList", list);
+		return map;
+	}
+	// 강의실 추가 
+	public void addClassroom(Classroom classroom, String instCode) {
+		System.out.println("[ClassroomService addClassroom]");
+		// 1. 강의실 테이블에 추가할 PK 구하는 코드 
+		// 1-1. 가져온 pk를 string 데이터타입의 변수에 담는다
+		String classroomPk = classroomMapper.selectClassroomPK();
+		System.out.println("[ClassroomService addClassroom] 가져온 classroomPk: "+classroomPk);
+		// 1-2. 이를 substring을 사용해 문자열을 자르고 숫자만 추출한 뒤, integer를 사용하여 int로 바꾸고, 얻은 숫자값에 1을 더한다
+		int lastNo = Integer.parseInt(classroomPk.substring(2));
+		System.out.println("[ClassroomService addClassroom] 추출한 lastNo: "+lastNo);
+		lastNo++;
+		System.out.println("[ClassroomService addClassroom] 1을더한 lastNo: "+lastNo);
+		// 1-3. 테이블 형식에 맞게 변수를 선언하고  이를 vo에 담는다. 
+		String crPK = "CR"+lastNo;
+		classroom.setClassroomCode(crPK);
+		System.out.println("[ClassroomService addClassroom] 최종 crPK: "+crPK);
+		
+		// 2. classroom 테이블에 강의실 등록하기
+		// 교육원검색화면에서 받아 등록화면에서 넘어온 institutionCode로 교육원명, 위치 정보 가져오고 이를 vo에 담아서 mapper호출하기
+		Map<String, Object> map = classroomMapper.selectInstitutionInfo(instCode);
+		String instName = (String)map.get("institutionName");
+		String instLocation = (String)map.get("institutionLocation");
+		System.out.println("[ClassroomService addClassroom] vo에 넣을 instName: "+instName);
+		System.out.println("[ClassroomService addClassroom] vo에 넣을 instLocation: "+instLocation);
+		classroom.setInstitutionName(instName);
+		classroom.setClassroomLocation(instLocation);
+		
+		classroomMapper.insertClassroom(classroom);
+	}
+		
 }
