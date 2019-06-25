036a15f6d30c4d40e7e9f744a0c5f8f495df2e13
Administrator <Administrator@smart20121>
2019-06-25 12:25:40 +0900

    [한병권] MemberOnline VO 수정완료

--- lms/src/main/java/kr/or/ksmart/lms/association/vo/MemberOnline.java
+++ lms/src/main/java/kr/or/ksmart/lms/association/vo/MemberOnline.java
@@ -5,75 +5,6 @@ public class MemberOnline {
 	private String memberCode;
 	private String memberName;
 	private String memberOnlineId;
-<<<<<<< HEAD
-	private String memberOnlinePw;
-	private String memberRank;
-	private String institutionCode;
-	private String institutionName;
-	private String memberOnlineDate;
-	
-	public String getMemberOnlineCode() {
-		return memberOnlineCode;
-	}
-	public void setMemberOnlineCode(String memberOnlineCode) {
-		this.memberOnlineCode = memberOnlineCode;
-	}
-	public String getMemberCode() {
-		return memberCode;
-	}
-	public void setMemberCode(String memberCode) {
-		this.memberCode = memberCode;
-	}
-	public String getMemberName() {
-		return memberName;
-	}
-	public void setMemberName(String memberName) {
-		this.memberName = memberName;
-	}
-	public String getMemberOnlineId() {
-		return memberOnlineId;
-	}
-	public void setMemberOnlineId(String memberOnlineId) {
-		this.memberOnlineId = memberOnlineId;
-	}
-	public String getMemberOnlinePw() {
-		return memberOnlinePw;
-	}
-	public void setMemberOnlinePw(String memberOnlinePw) {
-		this.memberOnlinePw = memberOnlinePw;
-	}
-	public String getMemberRank() {
-		return memberRank;
-	}
-	public void setMemberRank(String memberRank) {
-		this.memberRank = memberRank;
-	}
-	public String getInstitutionCode() {
-		return institutionCode;
-	}
-	public void setInstitutionCode(String institutionCode) {
-		this.institutionCode = institutionCode;
-	}
-	public String getInstitutionName() {
-		return institutionName;
-	}
-	public void setInstitutionName(String institutionName) {
-		this.institutionName = institutionName;
-	}
-	public String getMemberOnlineDate() {
-		return memberOnlineDate;
-	}
-	public void setMemberOnlineDate(String memberOnlineDate) {
-		this.memberOnlineDate = memberOnlineDate;
-	}
-	
-	@Override
-	public String toString() {
-		return "MemberOnline [memberOnlineCode=" + memberOnlineCode + ", memberCode=" + memberCode + ", memberName="
-				+ memberName + ", memberOnlineId=" + memberOnlineId + ", memberOnlinePw=" + memberOnlinePw
-				+ ", memberRank=" + memberRank + ", institutionCode=" + institutionCode + ", institutionName="
-				+ institutionName + ", memberOnlineDate=" + memberOnlineDate + "]";
-=======
 	private String memberRank;
 	private String institutionCode;
 	private String institutionName;
@@ -124,7 +55,6 @@ public class MemberOnline {
 		return "MemberOnline [memberOnlineCode=" + memberOnlineCode + ", memberCode=" + memberCode + ", memberName="
 				+ memberName + ", memberOnlineId=" + memberOnlineId + ", memberRank=" + memberRank
 				+ ", institutionCode=" + institutionCode + ", institutionName=" + institutionName + "]";
->>>>>>> refs/remotes/origin/hbg
 	}
 	
 }
