35112da00fa926235b2d57dea83f3e17afa82187
smart-202-15 <smart-202-15@smart-202-15-PC>
2019-06-26 11:22:10 +0900

    [나현주] 아래 commit 내용 동일 생성파일

---
+++ lms/src/main/java/kr/or/ksmart/lms/teacher/vo/TeacherMember.java
@@ -0,0 +1,55 @@
+package kr.or.ksmart.lms.teacher.vo;
+
+public class TeacherMember {
+	private String memberTeacherCode;
+	private String teacherApprovalCode;
+	private String memberCode;
+	private String memberTeacherName;
+	private String memberTeacherCareer;
+	private String memberTeacherServiceCheck;
+	
+	public String getMemberTeacherCode() {
+		return memberTeacherCode;
+	}
+	public void setMemberTeacherCode(String memberTeacherCode) {
+		this.memberTeacherCode = memberTeacherCode;
+	}
+	public String getTeacherApprovalCode() {
+		return teacherApprovalCode;
+	}
+	public void setTeacherApprovalCode(String teacherApprovalCode) {
+		this.teacherApprovalCode = teacherApprovalCode;
+	}
+	public String getMemberCode() {
+		return memberCode;
+	}
+	public void setMemberCode(String memberCode) {
+		this.memberCode = memberCode;
+	}
+	public String getMemberTeacherName() {
+		return memberTeacherName;
+	}
+	public void setMemberTeacherName(String memberTeacherName) {
+		this.memberTeacherName = memberTeacherName;
+	}
+	public String getMemberTeacherCareer() {
+		return memberTeacherCareer;
+	}
+	public void setMemberTeacherCareer(String memberTeacherCareer) {
+		this.memberTeacherCareer = memberTeacherCareer;
+	}
+	public String getMemberTeacherServiceCheck() {
+		return memberTeacherServiceCheck;
+	}
+	public void setMemberTeacherServiceCheck(String memberTeacherServiceCheck) {
+		this.memberTeacherServiceCheck = memberTeacherServiceCheck;
+	}
+	
+	@Override
+	public String toString() {
+		return "TeacherMember [memberTeacherCode=" + memberTeacherCode + ", teacherApprovalCode=" + teacherApprovalCode
+				+ ", memberCode=" + memberCode + ", memberTeacherName=" + memberTeacherName + ", memberTeacherCareer="
+				+ memberTeacherCareer + ", memberTeacherServiceCheck=" + memberTeacherServiceCheck + "]";
+	}
+	
+}
