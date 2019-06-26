a862cc752d80c570fb0b205aa09ec681477549b8
Administrator <Administrator@MSDN-SPECIAL>
2019-06-26 17:12:26 +0900

    [유하람] notice_lecture 관련 작업 시작
    - 과목공고 출력을 위한 준비
    - controller에서 mav에 해당 list들 담는 작업 해야함

--- lms/src/main/java/kr/or/ksmart/lms/association/mapper/AssociationInstitutionMapper.java
+++ lms/src/main/java/kr/or/ksmart/lms/association/mapper/AssociationInstitutionMapper.java
@@ -9,6 +9,12 @@ import kr.or.ksmart.lms.association.vo.Institution;
 @Mapper
 public interface AssociationInstitutionMapper {
 	
+	// 교육원 등록 
+	// 1. PK를 골라와서 새로운 코드 만드는 메서드
+	public String associationSelectInstitutionPK();
+	// 2. 등록 메서드
+	public void associationInsertInstitution(Institution institution);
+	
 	// 교욱원 목록 출력 메서드
 	public List<Institution> associationSelectInstitutionList();
 
---
+++ lms/src/main/java/kr/or/ksmart/lms/association/service/AssociationNoticeLectureService.java
@@ -0,0 +1,8 @@
+package kr.or.ksmart.lms.association.service;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class AssociationNoticeLectureService {
+
+}
