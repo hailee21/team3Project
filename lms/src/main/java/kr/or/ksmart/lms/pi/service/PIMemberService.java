f59c9bd297c92ccacd7d2d0212e232f0486e2e8b
smart-202-15 <smart-202-15@smart-202-15-PC>
2019-06-28 17:33:40 +0900

    [나현주] 레이아웃 수정

--- lms/src/main/java/kr/or/ksmart/lms/pi/service/PIMemberService.java
+++ lms/src/main/java/kr/or/ksmart/lms/pi/service/PIMemberService.java
@@ -2,9 +2,7 @@ package kr.or.ksmart.lms.pi.service;
 
 import java.text.SimpleDateFormat;
 import java.util.Date;
-import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
