d2fc3ebec3a8142cdb02987a77f75ecca12de423
smart-202-15 <smart-202-15@smart-202-15-PC>
2019-07-08 17:11:52 +0900

    [나현주] 게시판-커뮤니티 list 출력

--- lms/src/main/java/kr/or/ksmart/lms/pi/controller/PIBoardController.java
+++ lms/src/main/java/kr/or/ksmart/lms/pi/controller/PIBoardController.java
@@ -1,13 +1,17 @@
 package kr.or.ksmart.lms.pi.controller;
 
+import java.util.List;
+
 import javax.servlet.http.HttpSession;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
 
+import kr.or.ksmart.lms.association.vo.Board;
 import kr.or.ksmart.lms.index.vo.IndexInstitution;
 import kr.or.ksmart.lms.pi.service.PIBoardService;
 
@@ -21,9 +25,11 @@ public class PIBoardController {
 		IndexInstitution institution = piBoardService.PIIndex(institutionCode);
 		mav.addObject("institutionCode", institution.getInstitutionCode());
 		mav.addObject("institutionName", institution.getInstitutionName());
+		
 		mav.setViewName("PI/Board/PINotice");
 		return mav;
 	}
+	//	교육원/협회 공지사항 글쓰기 화면 get요청
 	@GetMapping("/PINoticeWrite")
 	public ModelAndView piWriteNotice (HttpSession session, ModelAndView mav
 			, @RequestParam(value="institutionCode", required = true) String institutionCode) {
@@ -46,10 +52,12 @@ public class PIBoardController {
 		IndexInstitution institution = piBoardService.PIIndex(institutionCode);
 		mav.addObject("institutionCode", institution.getInstitutionCode());
 		mav.addObject("institutionName", institution.getInstitutionName());
+		List<Board> boardList = piBoardService.getBoardList();
+		mav.addObject("boardList", boardList);
 		mav.setViewName("PI/Board/PIBoard");
 		return mav;
 	}
-	//	교육원홈페이지 커뮤니티 글쓰기 get요청
+	//	교육원홈페이지 커뮤니티 글쓰기 화면 get요청
 	@GetMapping("/PIBoardwrite")
 	public ModelAndView piWriteBoard (HttpSession session, ModelAndView mav
 			, @RequestParam(value="institutionCode", required = true) String institutionCode) {
@@ -65,6 +73,16 @@ public class PIBoardController {
 		}
 		return mav;
 	}
+	//	교육원홈페이지 커뮤니티 글쓰기 처리 POST 요청
+	@PostMapping("/addBoard")
+	public ModelAndView addBoard (Board board, HttpSession session, ModelAndView mav
+	, @RequestParam(value="institutionCode", required = true) String institutionCode) {
+		System.out.println("[PIBoardController addBoard] 글등록 처리요청");
+		piBoardService.addBoard(board, session, institutionCode);
+		mav.setViewName("PI/Board/PIBoard?institutionCode="+institutionCode);
+		return mav;
+	}
+	
 	//	교육원홈페이지 질의응답 view
 	@GetMapping("/PIQnA")
 	public ModelAndView getBoardByPIQnA (HttpSession session, ModelAndView mav
--- lms/src/main/java/kr/or/ksmart/lms/pi/service/PIBoardService.java
+++ lms/src/main/java/kr/or/ksmart/lms/pi/service/PIBoardService.java
@@ -1,15 +1,42 @@
 package kr.or.ksmart.lms.pi.service;
 
+import java.util.List;
+
+import javax.servlet.http.HttpSession;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import kr.or.ksmart.lms.association.vo.Board;
 import kr.or.ksmart.lms.index.vo.IndexInstitution;
 import kr.or.ksmart.lms.pi.mapper.PIBoardMapper;
 
 @Service
 public class PIBoardService {
 	@Autowired private PIBoardMapper piBoardMapper;
+	
 	public IndexInstitution PIIndex(String institutionCode) {
 		return piBoardMapper.selectInstitution(institutionCode);
+	}
+	//	게시판 Board 리스트 출력
+	public List<Board> getBoardList() {
+		List<Board> board = piBoardMapper.getBoard();
+		return board;
+	}
+	public void addBoard(Board board, HttpSession session, String institutionCode) {
+		System.out.println("[PIBoardService addBoard] Service 호출");
+		String memberOnlineCode = (String)session.getAttribute("memberOnlineCode");
+		String memberOnlineId = (String)session.getAttribute("memberOnlineId");
+		String memberName = (String)session.getAttribute("memberName");
+		String instCode = (String)session.getAttribute("institutionCode");
+		String institutionName = (String)session.getAttribute("institutionName");
+		board.setMemberOnlineCode(memberOnlineCode);
+		board.setMemberOnlineId(memberOnlineId);
+		board.setMemberName(memberName);
+		board.setInstitutionCode(instCode);
+		board.setInstitutionName(institutionName);
+		System.out.println(board.getBoardTitle());
+		
+		piBoardMapper.insertBoard(board);
 	}
 }
